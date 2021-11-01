480010cd043d6dd439d067c6658b7b1af98e33a7
Martina-dP <martudelpino@gmail.com>
2021-10-31 21:07:23 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -37,6 +37,7 @@ conn.sync({ force: true }).then(async() => {
   })
   await Country.bulkCreate(apiInfo);
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at 3000'); // eslint-disable-line no-console
   });
 });
+
